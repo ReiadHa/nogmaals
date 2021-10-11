b901d88c27a453b709d865104fda94937268eeed
Riad Hak <99054420@mydavinci.nl>
2021-10-11 11:13:36 +0200

    raden.py

---
+++ duizelig/raden.py
@@ -0,0 +1,25 @@
+import random
+
+number = random.randint(1,1000)
+round = []
+score = 0
+for x in range(1,21):
+ round = ('this is round: {}'.format(x))
+ print (round)
+ for i in range(0,10):
+    user = int(input('guess the number: '))
+    if abs(user - number) <= 50 and abs(user - number) > 20 :
+        print('hot')
+    if abs(user - number) <= 20:
+        print('very hot')  
+    if user == number:
+        print('Hurray')
+        print(f"you guessed the right numbber, its {number}")
+        print(round)
+        score = score +1
+        break
+ if user != number:
+    print(f'your guessed the incorrect number, the correct number is: {number} ')
+
+ number = random.randint(1,1000)
+print('Your score is: ',score)
